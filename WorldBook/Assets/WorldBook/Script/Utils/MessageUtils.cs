2c0110279379cbfbec6dffdb38a3a6a4fdbc5552
j0schiTadamori <j0schitadamori@gmail.com>
2019-03-24 19:49:19 +0300

    Внес нектороые шаги по процессу разработки.

---
+++ WorldBook/Assets/WorldBook/Script/Utils/MessageUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Инструменты для работы с выводом текста.
+/// </summary>
+public static class MessageUtils
+{
+
+}
